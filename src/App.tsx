f2163dd6428c478696a9c8cda8e6eca807ac31f7
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-08-11 14:29:20 +0900

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TagsField from "./components/TagsField";
+
+const TopPage = ({ searchQuery }: { searchQuery: string }) => (
+  <Container as="main" maxW="container.md" mt={4}>
+    <Link to="/all-twoshots">ALL TWOSHOTs</Link>
+    <TagsField searchQuery={searchQuery} />
+  </Container>
+);
+
+const AllTwoshotsPage = () => (
+  <>
+    <Heading>ALL TWOSHOTs</Heading>
+    <Link to="/">トップページ</Link>
+  </>
+);
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -10,9 +25,12 @@ function App() {
   return (
     <Box display="flex" flexDirection="column" minH="100vh">
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <Container as="main" maxW="container.md" mt={4}>
-        <TagsField searchQuery={searchQuery} />
-      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TopPage searchQuery={searchQuery} />} />
+          <Route path="/all-twoshots" element={<AllTwoshotsPage />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </Box>
   );

95f09e59bd3a800d420f3b598149b36e762d8a9b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-08-11 15:20:34 +0900

    allTagsをAppコンポーネントで保持

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Box, Container, Heading } from "@chakra-ui/react";
 import { useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import onesJson from "./assets/ones.json";
+import topicsJson from "./assets/topics.json";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TagsField from "./components/TagsField";
+import { TagType } from "./types";
+import { shuffle } from "./util";
 
-const TopPage = ({ searchQuery }: { searchQuery: string }) => (
+const TopPage = ({
+  searchQuery,
+  allTags,
+}: {
+  searchQuery: string;
+  allTags: TagType[];
+}) => (
   <Container as="main" maxW="container.md" mt={4}>
     <Link to="/all-twoshots">ALL TWOSHOTs</Link>
-    <TagsField searchQuery={searchQuery} />
+    <TagsField searchQuery={searchQuery} allTags={allTags} />
   </Container>
 );
 
@@ -21,13 +31,17 @@ const AllTwoshotsPage = () => (
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [allTags] = useState(shuffle(onesJson.concat(topicsJson)));
 
   return (
     <Box display="flex" flexDirection="column" minH="100vh">
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TopPage searchQuery={searchQuery} />} />
+          <Route
+            path="/"
+            element={<TopPage searchQuery={searchQuery} allTags={allTags} />}
+          />
           <Route path="/all-twoshots" element={<AllTwoshotsPage />} />
         </Routes>
       </BrowserRouter>
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,8 @@ export type SearchFieldType = {
   searchQuery: string;
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 };
+
+export type TagType = {
+  name: string;
+  mainIds: number[];
+};

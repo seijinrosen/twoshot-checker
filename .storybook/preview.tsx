0bdacd0c820de58dc4d2b7bf770d7415fea19c4e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2025-02-09 13:51:14 +0900

    Add ChakraProvider decorator to Storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,20 @@
+// https://storybook.js.org/docs/writing-stories/mocking-data-and-modules/mocking-providers
+
 import type { Preview } from "@storybook/react";
 
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../src/theme";
+
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ChakraProvider theme={theme}>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
